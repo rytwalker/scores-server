4c4c16701e6c17ae14128240c029d7a65ae431db
rytwalker <rytwalker@gmail.com>
2018-11-18 06:50:42 -0500

    Added endpoint to show team and average total score

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const express = require('express');
+const knex = require('knex');
+const knexConfig = require('./knexfile');
+
+const db = knex(knexConfig.development);
 const server = express();
+
+server.get('/', async (req, res) => {
+  try {
+    const teams = await db
+      .select('t.name as Team')
+      .avg('s.total_points_scored as Average Score ')
+      .groupBy('Team')
+      .orderBy('Average Score', 'desc')
+      .from('teams as t')
+      .join('scores as s', 't.id', 's.team_id');
+
+    res.status(200).json(teams);
+  } catch (errors) {
+    res.status(500).json({ errors: 'There was an error.' });
+  }
+});
 
 const port = process.env.PORT || 5000;
 
