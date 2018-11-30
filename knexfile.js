11d681efc6651e7a815bb36761ffa279ae7016ae
rytwalker <rytwalker@gmail.com>
2018-11-30 01:50:34 -0500

    Added ssh

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,5 @@
 // Update with your config settings.
-// require('dotenv').config();
+require('dotenv').config();
 
 module.exports = {
   development: {
@@ -18,7 +18,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL,
+    connection: process.env.DATABASE_URL + '?ssl=true',
     pool: {
       min: 2,
       max: 10
